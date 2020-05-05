e7bd0ac9c48215f1ad53c99d9bedac9caf6f7aba
Abedalkareem <abedalkareem.omreyh@yahoo.com>
2020-05-05 08:41:16 +0400

    handle explicitSignIn result

--- android/src/main/kotlin/com/abedalkareem/games_services/GamesServicesPlugin.kt
+++ android/src/main/kotlin/com/abedalkareem/games_services/GamesServicesPlugin.kt
@@ -1,14 +1,17 @@
 package com.abedalkareem.games_services
 
 import android.app.Activity
+import android.content.Intent
 import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.games.AchievementsClient
 import com.google.android.gms.games.Games
 import com.google.android.gms.games.LeaderboardsClient
-import android.widget.Toast
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
@@ -17,159 +20,207 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.ActivityResultListener
+
 
 private const val CHANNEL_NAME = "games_services"
+private const val RC_SIGN_IN = 9000
 
-class GamesServicesPlugin(private var activity: Activity? = null) : FlutterPlugin, MethodCallHandler, ActivityAware {
+class GamesServicesPlugin(private var activity: Activity? = null) : FlutterPlugin, MethodCallHandler, ActivityAware, ActivityResultListener {
 
-    //region Variables
-    private var googleSignInClient: GoogleSignInClient? = null
-    private var achievementClient: AchievementsClient? = null
-    private var leaderboardsClient: LeaderboardsClient? = null
+  //region Variables
+  private var googleSignInClient: GoogleSignInClient? = null
+  private var achievementClient: AchievementsClient? = null
+  private var leaderboardsClient: LeaderboardsClient? = null
+  private var activityPluginBinding: ActivityPluginBinding? = null
+  private var channel: MethodChannel? = null
+  private var pendingOperation: PendingOperation? = null
+  //endregion
 
-    private var channel: MethodChannel? = null
-    //endregion
-
-    private fun explicitSignIn() {
-        val activity = activity ?: return
-        val builder = GoogleSignInOptions.Builder(
-                GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
-        builder.requestEmail()
-        googleSignInClient = GoogleSignIn.getClient(activity, builder.build())
-        activity.startActivityForResult(googleSignInClient?.signInIntent, 0)
+  //region SignIn
+  private fun silentSignIn(result: Result) {
+    val activity = activity ?: return
+    val builder = GoogleSignInOptions.Builder(
+            GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+    builder.requestEmail()
+    googleSignInClient = GoogleSignIn.getClient(activity, builder.build())
+    googleSignInClient?.silentSignIn()?.addOnCompleteListener { task ->
+      val googleSignInAccount = task.result
+      pendingOperation = PendingOperation(Methods.silentSignIn, result)
+      if (task.isSuccessful && googleSignInAccount != null) {
+        handleSignInResult(googleSignInAccount)
+      } else {
+        Log.e("Error", "signInError", task.exception)
+        Log.i("ExplicitSignIn", "Trying explicit sign in")
+        explicitSignIn()
+      }
     }
+  }
 
-    //region SignIn
-    private fun silentSignIn(result: Result) {
-        val activity = activity ?: return
-        val builder = GoogleSignInOptions.Builder(
-                GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
-        builder.requestEmail()
-        googleSignInClient = GoogleSignIn.getClient(activity, builder.build())
-        googleSignInClient?.silentSignIn()?.addOnCompleteListener { task ->
-            try {
-                val googleSignInAccount = task.result
-                if (task.isSuccessful && googleSignInAccount != null) {
-                    achievementClient = Games.getAchievementsClient(activity, googleSignInAccount)
-                    leaderboardsClient = Games.getLeaderboardsClient(activity, googleSignInAccount)
-                    result.success("success")
-                } else {
-                    Log.e("Error", "signInError", task.exception)
-                    result.error("error", task.exception?.message ?: "", null)
-                }
-            }catch(ex: Exception) {
-                explicitSignIn()
-                result.success("success")
+  private fun explicitSignIn() {
+    val activity = activity ?: return
+    val builder = GoogleSignInOptions.Builder(
+            GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+    builder.requestEmail()
+    googleSignInClient = GoogleSignIn.getClient(activity, builder.build())
+    activity.startActivityForResult(googleSignInClient?.signInIntent, RC_SIGN_IN)
+  }
 
-            }
+  private fun handleSignInResult(googleSignInAccount: GoogleSignInAccount) {
+    achievementClient = Games.getAchievementsClient(activity!!, googleSignInAccount)
+    leaderboardsClient = Games.getLeaderboardsClient(activity!!, googleSignInAccount)
+    finishPendingOperationWithSuccess()
+  }
+  //endregion
+
+  //region Achievements
+  private fun showAchievements(result: Result) {
+    achievementClient?.achievementsIntent?.addOnSuccessListener { intent ->
+      activity?.startActivityForResult(intent, 0)
+      result.success("success")
+    }?.addOnFailureListener {
+      result.error("error", "${it.message}", null)
+    }
+  }
+
+  private fun unlock(achievementID: String, result: Result) {
+    achievementClient?.unlock(achievementID)
+    result.success("success")
+  }
+  //endregion
+
+  //region Leaderboards
+  private fun showLeaderboards(result: Result) {
+    if (leaderboardsClient?.allLeaderboardsIntent == null) {
+      Toast.makeText(activity, "Please log to Google Play Games", Toast.LENGTH_LONG).show()
+    }
+    leaderboardsClient?.allLeaderboardsIntent?.addOnSuccessListener { intent ->
+      activity?.startActivityForResult(intent, 0)
+      result.success("success")
+    }?.addOnFailureListener {
+      result.error("error", "${it.message}", null)
+    }
+  }
+
+  private fun submitScore(leaderboardID: String, score: Int, result: Result) {
+    leaderboardsClient?.submitScore(leaderboardID, score.toLong())
+    result.success("success")
+  }
+  //endregion
+
+  //region FlutterPlugin
+  override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    setupChannel(binding.binaryMessenger)
+  }
+
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    teardownChannel()
+  }
+
+  private fun setupChannel(messenger: BinaryMessenger) {
+    channel = MethodChannel(messenger, CHANNEL_NAME)
+    val handler = GamesServicesPlugin(activity)
+    channel?.setMethodCallHandler(handler)
+  }
+
+  private fun teardownChannel() {
+    channel?.setMethodCallHandler(null)
+    channel = null
+  }
+  //endregion
+
+  //region ActivityAware
+
+  private fun disposeActivity() {
+    activityPluginBinding?.removeActivityResultListener(this)
+    activityPluginBinding = null
+  }
+
+  override fun onDetachedFromActivity() {
+    disposeActivity()
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    onAttachedToActivity(binding)
+  }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activityPluginBinding = binding
+    activity = binding.activity
+    binding.addActivityResultListener(this)
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    onDetachedFromActivity()
+  }
+  //endregion
+
+  //region PendingOperation
+  private class PendingOperation constructor(val method: String, val result: Result)
+
+  private fun finishPendingOperationWithSuccess() {
+    Log.i(pendingOperation!!.method, "success")
+    pendingOperation!!.result.success("success")
+    pendingOperation = null
+  }
+
+  private fun finishPendingOperationWithError(errorMessage: String) {
+    Log.i(pendingOperation!!.method, "error")
+    pendingOperation!!.result.error("error", errorMessage, null)
+    pendingOperation = null
+  }
+  //endregion
+
+  //region ActivityResultListener
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
+    if (requestCode == RC_SIGN_IN) {
+      val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+      val signInAccount = result.signInAccount
+      if (result.isSuccess && signInAccount != null) {
+        handleSignInResult(signInAccount)
+      } else {
+        var message = result.status.statusMessage ?: ""
+        if (message.isEmpty()) {
+          message = "Something went wrong " + result.status
         }
+        finishPendingOperationWithError(message)
+      }
+      return true
     }
-    //endregion
+    return false
+  }
+  //endregion
 
-    //region Achievements
-    private fun showAchievements(result: Result) {
-        achievementClient?.achievementsIntent?.addOnSuccessListener { intent ->
-            activity?.startActivityForResult(intent, 0)
-            result.success("success")
-        }?.addOnFailureListener {
-            result.error("error", "${it.message}", null)
-        }
+  //region MethodCallHandler
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    when (call.method) {
+      Methods.unlock -> {
+        unlock(call.argument<String>("achievementID") ?: "", result)
+      }
+      Methods.submitScore -> {
+        val leaderboardID = call.argument<String>("leaderboardID") ?: ""
+        val score = call.argument<Int>("value") ?: 0
+        submitScore(leaderboardID, score, result)
+      }
+      Methods.showLeaderboards -> {
+        showLeaderboards(result)
+      }
+      Methods.showAchievements -> {
+        showAchievements(result)
+      }
+      Methods.silentSignIn -> {
+        silentSignIn(result)
+      }
+      else -> result.notImplemented()
     }
+  }
+  //endregion
+}
 
-    private fun unlock(achievementID: String, result: Result) {
-        achievementClient?.unlock(achievementID)
-        result.success("success")
-    }
-    //endregion
-
-    //region Leaderboards
-    private fun showLeaderboards(result: Result) {
-        if (leaderboardsClient?.allLeaderboardsIntent == null) {
-            Toast.makeText(activity, "Please log to Google Play Games", Toast.LENGTH_LONG).show()
-        }
-        leaderboardsClient?.allLeaderboardsIntent?.addOnSuccessListener { intent ->
-            activity?.startActivityForResult(intent, 0)
-            result.success("success")
-        }?.addOnFailureListener {
-            result.error("error", "${it.message}", null)
-        }
-    }
-
-    private fun submitScore(leaderboardID: String, score: Int, result: Result) {
-        leaderboardsClient?.submitScore(leaderboardID, score.toLong())
-        result.success("success")
-    }
-    //endregion
-
-    //region FlutterPlugin
-    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        setupChannel(binding.binaryMessenger)
-    }
-
-    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        teardownChannel()
-    }
-
-    private fun setupChannel(messenger: BinaryMessenger) {
-        channel = MethodChannel(messenger, CHANNEL_NAME)
-        val handler = GamesServicesPlugin(activity)
-        channel?.setMethodCallHandler(handler)
-    }
-
-    private fun teardownChannel() {
-        channel?.setMethodCallHandler(null)
-        channel = null
-    }
-    //endregion
-
-    //region ActivityAware
-    override fun onDetachedFromActivity() {
-        teardownChannel()
-    }
-
-    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-        onAttachedToActivity(binding)
-    }
-
-    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-        activity = binding.activity
-
-        // if channel was already created update it
-        if (channel != null) {
-            val handler = GamesServicesPlugin(binding.activity)
-            channel?.setMethodCallHandler(handler)
-        }
-    }
-
-    override fun onDetachedFromActivityForConfigChanges() {
-        onDetachedFromActivity()
-    }
-    //endregion
-
-    //region MethodCallHandler
-    override fun onMethodCall(call: MethodCall, result: Result) {
-        when (call.method) {
-            "unlock" -> {
-                unlock(call.argument<String>("achievementID") ?: "", result)
-            }
-            "submitScore" -> {
-                val leaderboardID = call.argument<String>("leaderboardID") ?: ""
-                val score = call.argument<Int>("value") ?: 0
-                submitScore(leaderboardID, score, result)
-            }
-            "showLeaderboards" -> {
-                showLeaderboards(result)
-            }
-            "showAchievements" -> {
-                showAchievements(result)
-            }
-            "silentSignIn" -> {
-                silentSignIn(result)
-            }
-            else -> result.notImplemented()
-        }
-    }
-
-    //endregion
-
+object Methods {
+  const val unlock = "unlock"
+  const val submitScore = "submitScore"
+  const val showLeaderboards = "showLeaderboards"
+  const val showAchievements = "showAchievements"
+  const val silentSignIn = "silentSignIn"
 }
