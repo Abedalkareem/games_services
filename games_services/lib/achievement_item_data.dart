f0e4659ac82253fd1a1b6c4ad6542cc108216e82
abedalkareem omreyh <abedalkareem.omreyh@yahoo.com>
2022-08-30 07:36:45 +0400

    make some properties non nullable in AchievementItemData

--- games_services/lib/achievement_item_data.dart
+++ games_services/lib/achievement_item_data.dart
@@ -1,10 +1,10 @@
 class AchievementItemData {
-  final String? id;
-  final String? name;
+  final String id;
+  final String name;
 
   /// On iOS [description] will be [achievedDescription] in case chievement completed else it
   /// will be [unachievedDescription].
-  final String? description;
+  final String description;
 
   /// The achievement locked (revealed) image as base64.
   final String? lockedImage;
@@ -14,23 +14,23 @@ class AchievementItemData {
 
   /// On android the achievement type should be [TYPE_INCREMENTAL] to get the steps, else it will be 0.
   /// On iOS [completedSteps] is same as [percentComplete].
-  final int? completedSteps;
+  final int completedSteps;
 
   /// On android the achievement type should be [TYPE_INCREMENTAL] to get the total steps, else it will be 0.
   /// On iOS [totalSteps] will have always 100.
-  final int? totalSteps;
+  final int totalSteps;
 
-  final bool? unlocked;
+  final bool unlocked;
 
   const AchievementItemData({
-    this.id,
-    this.name,
-    this.description,
+    required this.id,
+    required this.name,
+    required this.description,
     this.lockedImage,
     this.unlockedImage,
-    this.completedSteps,
-    this.totalSteps,
-    this.unlocked,
+    required this.completedSteps,
+    required this.totalSteps,
+    required this.unlocked,
   });
 
   factory AchievementItemData.fromJson(Map<String, dynamic> json) {
