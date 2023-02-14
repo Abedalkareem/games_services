21679ae989cd01dfd2e9765bbaf9e88e7426e5a9
Anton Holovin <antnglvn@gmail.com>
2023-02-14 23:58:39 +0200

    Replace addOnCompleteListener with addOnSuccessListener in SaveGame.kt

--- games_services/android/src/main/kotlin/com/abedalkareem/games_services/SaveGame.kt
+++ games_services/android/src/main/kotlin/com/abedalkareem/games_services/SaveGame.kt
@@ -23,16 +23,16 @@ class SaveGame(private var activityPluginBinding: ActivityPluginBinding) {
 
   fun getSavedGames(result: MethodChannel.Result) {
     snapshotsClient?.load(true)
-      ?.addOnCompleteListener { task ->
+      ?.addOnSuccessListener { snapshotResult ->
         val gson = Gson()
-        val data = task.result.get()
+        val data = snapshotResult.get()
         if (data == null) {
           result.error(
             PluginError.FailedToGetSavedGames.errorCode(),
             PluginError.FailedToGetSavedGames.errorMessage(),
             null
           )
-          return@addOnCompleteListener
+          return@addOnSuccessListener
         }
         val items = data
           .toList()
