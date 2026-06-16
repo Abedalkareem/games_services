5bd652dffb7ae74cd453f486377546dbb1ec0725
theLee3 <46857988+theLee3@users.noreply.github.com>
2026-06-16 12:33:11 -0400

    Update chained async method calls to passthrough. Remove unnecessary type declarations.

--- games_services/lib/src/player.dart
+++ games_services/lib/src/player.dart
@@ -29,14 +29,12 @@ abstract class Player {
   );
 
   /// Show the Game Center Access Point for the current player.
-  static Future<String?> showAccessPoint(AccessPointLocation location) async {
-    return await GamesServicesPlatform.instance.showAccessPoint(location);
-  }
+  static Future<String?> showAccessPoint(AccessPointLocation location) =>
+      GamesServicesPlatform.instance.showAccessPoint(location);
 
   /// Hide the Game Center Access Point.
-  static Future<String?> hideAccessPoint() async {
-    return await GamesServicesPlatform.instance.hideAccessPoint();
-  }
+  static Future<String?> hideAccessPoint() =>
+      GamesServicesPlatform.instance.hideAccessPoint();
 
   /// Get the current player's ID.
   /// On iOS/macOS the player ID is unique for your game but not other games.
@@ -72,19 +70,19 @@ abstract class Player {
 
   /// Get the player's hi-res profile image as a base64 encoded String.
   static Future<String?> getPlayerHiResImage() async =>
-      (await GamesServicesPlatform.instance.getPlayerHiResImage())
-          ?.replaceAll("\n", "");
+      (await GamesServicesPlatform.instance.getPlayerHiResImage())?.replaceAll(
+        "\n",
+        "",
+      );
 
   /// Get the current player's score for a specific leaderboard.
   static Future<int?> getPlayerScore({
     String iOSLeaderboardID = "",
     String androidLeaderboardID = "",
-  }) async {
-    return await GamesServicesPlatform.instance.getPlayerScore(
-      iOSLeaderboardID: iOSLeaderboardID,
-      androidLeaderboardID: androidLeaderboardID,
-    );
-  }
+  }) => GamesServicesPlatform.instance.getPlayerScore(
+    iOSLeaderboardID: iOSLeaderboardID,
+    androidLeaderboardID: androidLeaderboardID,
+  );
 
   /// Check if the current player is underage (always false on Android).
   static Future<bool?> get isUnderage async {
